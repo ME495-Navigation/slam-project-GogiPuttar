5f95fe681525878db07c641facfbce3f80e02717
Aditya Nair <aditya.nair0123@gmail.com>
2024-01-17 19:23:27 -0600

    Made some progress

--- nuturtle_description/launch/load_one.launch.py
+++ nuturtle_description/launch/load_one.launch.py
@@ -4,8 +4,8 @@ Starts all the nodes to visualize a robot in rviz
 
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import Command, PathJoinSubstitution, TextSubstitution, LaunchConfiguration
+from launch.actions import DeclareLaunchArgument, SetLaunchConfiguration, Shutdown
+from launch.substitutions import Command, PathJoinSubstitution, TextSubstitution, LaunchConfiguration, EqualsSubstitution
 from launch.conditions import IfCondition, LaunchConfigurationEquals
 from launch_ros.substitutions import FindPackageShare
 
@@ -17,30 +17,59 @@ def generate_launch_description():
         DeclareLaunchArgument(name="use_rviz", default_value="true",
                               description="true (default): start rviz, otherwise don't start rviz"),
 
+        DeclareLaunchArgument(name="color", default_value="purple",
+                              description="Color of nuturtle's body",
+                              choices=["red", "green", "blue", "purple"]),
+
+        SetLaunchConfiguration(name="rviz_color",
+                               value=[FindPackageShare("nuturtle_description"),
+                                      TextSubstitution(text="/config/basic"),
+                                      LaunchConfiguration("color"),
+                                      TextSubstitution(text=".rviz")]),
+
         Node(package="joint_state_publisher_gui",
+             namespace=LaunchConfiguration("color"),
              executable="joint_state_publisher_gui",
              condition= LaunchConfigurationEquals("use_jsp", "gui")
              ),
         Node(package="joint_state_publisher",
+             namespace=LaunchConfiguration("color"),
              executable="joint_state_publisher",
              condition= LaunchConfigurationEquals("use_jsp", "jsp")
              ),
         Node(
             package="robot_state_publisher",
             executable="robot_state_publisher",
+            namespace=LaunchConfiguration("color"),
             parameters=[
-                {"robot_description" :
+                { 
+                    "frame_prefix":
+                    PathJoinSubstitution([(LaunchConfiguration('color')), '']),
+                  "robot_description" :
                  Command([TextSubstitution(text="xacro "),
                           PathJoinSubstitution(
-                              [FindPackageShare("nuturtle_description"), "urdf", "turtlebot3_burger.urdf.xacro"])])}
+                              [FindPackageShare("nuturtle_description"), "urdf", "turtlebot3_burger.urdf.xacro"]),
+                          " color:=",
+                          LaunchConfiguration("color")])}
             ]
             ),
         Node(
             package="rviz2",
             executable="rviz2",
-            arguments=["-d",
+            namespace=LaunchConfiguration("color"),
+            arguments=["-d", 
                        PathJoinSubstitution(
                            [FindPackageShare("nuturtle_description"), "config", "basic_purple.rviz"])],
             condition=LaunchConfigurationEquals("use_rviz", "true")
-            )
+            ),
+
+        # Node(
+        #     package='rviz2',
+        #     executable='rviz2',
+        #     namespace=LaunchConfiguration("color"),
+        #     arguments=["-d", LaunchConfiguration("rviz_color")],
+        #     condition=IfCondition(EqualsSubstitution(
+        #         LaunchConfiguration("use_rviz"), "true")),
+        #     on_exit=Shutdown()
+        # ),
         ])
