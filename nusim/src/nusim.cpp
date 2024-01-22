b86b2992569c265ccf3696913fa5391cd4a3d663
Aditya Nair <aditya.nair0123@gmail.com>
2024-01-22 10:18:18 -0600

    Completed Task C.3

--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -12,8 +12,8 @@
 ///     \param obstacles.y (std::vector<double>): Vector of y coordinates for each obstacle [m]
 ///     \param obstacles.r (double): Radius of cylindrical obstacles [m]
 ///     \param obstacles.h (double): Height of cylindrical obstacles [m]
-///     \param walls.x_lenght (double): Inner lenght of walls in x direction [m]
-///     \param walls.y_lenght (double): Inner lenght of walls in y direction [m]
+///     \param walls.x_length (double): Inner length of walls in x direction [m]
+///     \param walls.y_length (double): Inner length of walls in y direction [m]
 ///     \param walls.h (double): Walls height [m]
 ///     \param walls.w (double): Walls width [m]
 ///
@@ -51,7 +51,7 @@
 #include "geometry_msgs/msg/transform_stamped.hpp"
 #include "tf2/LinearMath/Quaternion.h"
 #include "tf2_ros/transform_broadcaster.h"
-// #include "nusim/srv/teleport.hpp"
+#include "nusim/srv/teleport.hpp"
 #include "visualization_msgs/msg/marker_array.hpp"
 #include "visualization_msgs/msg/marker.hpp"
 #include "turtlelib/geometry2d.hpp"
@@ -91,8 +91,8 @@ std::mt19937 & get_random()
 ///  \param obstacles_y_ (std::vector<double>): Vector of y coordinates for each obstacle [m]
 ///  \param obstacles_r_ (double): Radius of cylindrical obstacles [m]
 ///  \param obstacles_h_ (double): Height of cylindrical obstacles [m]
-///  \param walls_x_lenght_ (double): Inner lenght of walls in x direction [m]
-///  \param walls_y_lenght_ (double): Inner lenght of walls in y direction [m]
+///  \param walls_x_length_ (double): Inner length of walls in x direction [m]
+///  \param walls_y_length_ (double): Inner length of walls in y direction [m]
 ///  \param walls_h_(double): Walls height [m]
 ///  \param walls_w_(double): Walls width [m]
 ///  \param wheelradius_ (double): The radius of the wheels [m]
@@ -102,13 +102,13 @@ class Nusim : public rclcpp::Node
 {
 public:
   Nusim()
-  : Node("Nusim"), timestep_(0)
+  : Node("nusim"), timestep_(0)
   {
     // Parameter descirption
     auto rate_des = rcl_interfaces::msg::ParameterDescriptor{};
-    // auto x0_des = rcl_interfaces::msg::ParameterDescriptor{};
-    // auto y0_des = rcl_interfaces::msg::ParameterDescriptor{};
-    // auto theta0_des = rcl_interfaces::msg::ParameterDescriptor{};
+    auto x0_des = rcl_interfaces::msg::ParameterDescriptor{};
+    auto y0_des = rcl_interfaces::msg::ParameterDescriptor{};
+    auto theta0_des = rcl_interfaces::msg::ParameterDescriptor{};
     // auto obstacles_x_des = rcl_interfaces::msg::ParameterDescriptor{};
     // auto obstacles_y_des = rcl_interfaces::msg::ParameterDescriptor{};
     // auto obstacles_r_des = rcl_interfaces::msg::ParameterDescriptor{};
@@ -122,9 +122,9 @@ public:
     // auto track_width_des = rcl_interfaces::msg::ParameterDescriptor{};
 
     rate_des.description = "Timer callback frequency [Hz]";
-    // x0_des.description = "Initial x coordinate of the robot [m]";
-    // y0_des.description = "Initial y coordinate of the robot [m]";
-    // theta0_des.description = "Initial theta angle of the robot [radians]";
+    x0_des.description = "Initial x coordinate of the robot [m]";
+    y0_des.description = "Initial y coordinate of the robot [m]";
+    theta0_des.description = "Initial theta angle of the robot [radians]";
     // obstacles_x_des.description = "Vector of x coordinates for each obstacle [m]";
     // obstacles_y_des.description = "Vector of y coordinates for each obstacle [m]";
     // obstacles_r_des.description = "Radius of cylindrical obstacles [m]";
@@ -140,15 +140,15 @@ public:
 
     // Declare default parameters values
     declare_parameter("rate", 200, rate_des);     // Hz for timer_callback
-    // declare_parameter("x0", 0.0, x0_des);
-    // declare_parameter("y0", 0.0, y0_des);
-    // declare_parameter("theta0", 0.0, theta0_des);
+    declare_parameter("x0", 0.0, x0_des);
+    declare_parameter("y0", 0.0, y0_des);
+    declare_parameter("theta0", 0.0, theta0_des);
     // declare_parameter("obstacles.x", std::vector<double>{}, obstacles_x_des);
     // declare_parameter("obstacles.y", std::vector<double>{}, obstacles_y_des);
     // declare_parameter("obstacles.r", 0.0, obstacles_r_des);
     // declare_parameter("obstacles.h", 0.0, obstacles_h_des);
-    // declare_parameter("walls.x_lenght", 0.0, walls_x_des);
-    // declare_parameter("walls.y_lenght", 0.0, walls_y_des);
+    // declare_parameter("walls.x_length", 0.0, walls_x_des);
+    // declare_parameter("walls.y_length", 0.0, walls_y_des);
     // declare_parameter("walls.h", 0.0, walls_h_des);
     // declare_parameter("walls.w", 0.0, walls_w_des);
     // declare_parameter("wheelradius", -1.0, wheelradius_des);
@@ -156,22 +156,22 @@ public:
 
     // Get params - Read params from yaml file that is passed in the launch file
     int rate = get_parameter("rate").get_parameter_value().get<int>();
-    // x0_ = get_parameter("x0").get_parameter_value().get<double>();
-    // y0_ = get_parameter("y0").get_parameter_value().get<double>();
-    // theta0_ = get_parameter("theta0").get_parameter_value().get<double>();
+    x0_ = get_parameter("x0").get_parameter_value().get<double>();
+    y0_ = get_parameter("y0").get_parameter_value().get<double>();
+    theta0_ = get_parameter("theta0").get_parameter_value().get<double>();
     // obstacles_x_ = get_parameter("obstacles.x").get_parameter_value().get<std::vector<double>>();
     // obstacles_y_ = get_parameter("obstacles.y").get_parameter_value().get<std::vector<double>>();
     // obstacles_r_ = get_parameter("obstacles.r").get_parameter_value().get<double>();
     // obstacles_h_ = get_parameter("obstacles.h").get_parameter_value().get<double>();
-    // walls_x_ = get_parameter("walls.x_lenght").get_parameter_value().get<double>();
-    // walls_y_ = get_parameter("walls.y_lenght").get_parameter_value().get<double>();
+    // walls_x_ = get_parameter("walls.x_length").get_parameter_value().get<double>();
+    // walls_y_ = get_parameter("walls.y_length").get_parameter_value().get<double>();
     // wall_h_ = get_parameter("walls.h").get_parameter_value().get<double>();
     // wall_w_ = get_parameter("walls.w").get_parameter_value().get<double>();
 
     // Set current robot pose equal to initial pose
-    // x_ = x0_;
-    // y_ = y0_;
-    // theta_ = theta0_;
+    x_ = x0_;
+    y_ = y0_;
+    theta_ = theta0_;
 
     // Timer timestep [seconds]
     dt_ = 1.0 / static_cast<double>(rate);
@@ -206,9 +206,9 @@ public:
       "~/reset",
       std::bind(&Nusim::reset_callback, this, std::placeholders::_1, std::placeholders::_2));
     // Teleport service
-    // teleport_server_ = create_service<nusim::srv::Teleport>(
-    //   "~/teleport",
-    //   std::bind(&Nusim::teleport_callback, this, std::placeholders::_1, std::placeholders::_2));
+    teleport_server_ = create_service<nusim::srv::Teleport>(
+      "~/teleport",
+      std::bind(&Nusim::teleport_callback, this, std::placeholders::_1, std::placeholders::_2));
 
     // Initialize the transform broadcaster
     tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
@@ -227,8 +227,8 @@ private:
   size_t timestep_;
   std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
   double x_, y_, theta_;     // Theta in radians, x & y in meters.
-  double x0_ = 0;
-  double y0_ = 0;
+  double x0_ = 0.0;
+  double y0_ = 0.0;
   double theta0_ = 0;
   double dt_ = 0.0; // Nusim Timer
 //   double obstacles_r_;    // Size of obstacles
@@ -260,7 +260,7 @@ private:
 //   rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr red_turtle_publisher_;
 //   rclcpp::Subscription<nuturtlebot_msgs::msg::WheelCommands>::SharedPtr red_wheel_cmd_subscriber_;
   rclcpp::Service<std_srvs::srv::Empty>::SharedPtr reset_server_;
-  // rclcpp::Service<nusim::srv::Teleport>::SharedPtr teleport_server_;
+  rclcpp::Service<nusim::srv::Teleport>::SharedPtr teleport_server_;
 
   /// \brief Subscription callback function for wheel_cmd topic
 //   void red_wheel_cmd_callback(const nuturtlebot_msgs::msg::WheelCommands & msg)
@@ -337,42 +337,42 @@ private:
   }
 
   /// \brief Teleport the robot to a specified pose
-//   void teleport_callback(
-//     nusim::srv::Teleport::Request::SharedPtr request,
-//     nusim::srv::Teleport::Response::SharedPtr)
-//   {
-//     x_ = request->x;
-//     y_ = request->y;
-//     theta_ = request->theta;
-//   }
+  void teleport_callback(
+    nusim::srv::Teleport::Request::SharedPtr request,
+    nusim::srv::Teleport::Response::SharedPtr)
+  {
+    x_ = request->x;
+    y_ = request->y;
+    theta_ = request->theta;
+  }
 
   /// \brief Broadcast the TF frames of the robot
-//   void broadcast_red_turtle()
-//   {
-//     geometry_msgs::msg::TransformStamped t_;
+  void broadcast_red_turtle()
+  {
+    geometry_msgs::msg::TransformStamped t_;
 
-//     t_.header.stamp = get_clock()->now();
-//     // t_.header.stamp.nanosec += 50000000; // TODO - Fixes the fake obstacle in Rviz
-//     t_.header.frame_id = "nusim/world";
-//     t_.child_frame_id = "red/base_footprint";
-//     t_.transform.translation.x = x_;
-//     t_.transform.translation.y = y_;
-//     t_.transform.translation.z = 0.0;     // Turtle only exists in 2D
+    t_.header.stamp = get_clock()->now();
+    // t_.header.stamp.nanosec += 50000000; // TODO - Fixes the fake obstacle in Rviz
+    t_.header.frame_id = "nusim/world";
+    t_.child_frame_id = "red/base_footprint";
+    t_.transform.translation.x = x_;
+    t_.transform.translation.y = y_;
+    t_.transform.translation.z = 0.0;     // Turtle only exists in 2D
 
-//     tf2::Quaternion q_;
-//     q_.setRPY(0, 0, theta_);     // Rotation around z-axis
-//     t_.transform.rotation.x = q_.x();
-//     t_.transform.rotation.y = q_.y();
-//     t_.transform.rotation.z = q_.z();
-//     t_.transform.rotation.w = q_.w();
+    tf2::Quaternion q_;
+    q_.setRPY(0, 0, theta_);     // Rotation around z-axis
+    t_.transform.rotation.x = q_.x();
+    t_.transform.rotation.y = q_.y();
+    t_.transform.rotation.z = q_.z();
+    t_.transform.rotation.w = q_.w();
 
-//     // Send the transformation
-//     tf_broadcaster_->sendTransform(t_);
+    // Send the transformation
+    tf_broadcaster_->sendTransform(t_);
 
-//     if (timestep_ % 100 == 1) {
-//       red_turtle_NavPath();
-//     }
-//   }
+    // if (timestep_ % 100 == 1) {
+    //   red_turtle_NavPath();
+    // }
+  }
 
   /// \brief Create the red turtle's nav_msgs/Path
 //   void red_turtle_NavPath()
@@ -400,7 +400,7 @@ private:
 //   void create_obstacles_array()
 //   {
 //     if (obstacles_x_.size() != obstacles_y_.size()) {
-//       throw std::runtime_error("x and y coordinate lists are not the same lenght!");
+//       throw std::runtime_error("x and y coordinate lists are not the same length!");
 //     }
 
 //     for (size_t i = 0; i < obstacles_x_.size(); i++) {
@@ -494,7 +494,7 @@ private:
     // if (draw_only_ == false) {
     //   update_red_turtle_config();
     //   sensor_data_publisher_->publish(sensor_data_);
-    //   broadcast_red_turtle();
+    broadcast_red_turtle();
     //   red_turtle_publisher_->publish(red_path_);
     // }
   }
